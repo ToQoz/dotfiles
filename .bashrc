835ebd9b5cbbb25abffa299a866b3d2daee95633
toqoz <toqoz403@gmail.com>
2011-11-25 13:21:46 +0900

    aliasとか (darwin

--- .bashrc
+++ .bashrc
@@ -12,24 +12,25 @@ darwin*)
     alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
     export EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
 
+    # emacs
     alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
+
     # MacFusion alias
     alias miacc='cd /Volumes/test_iacc'
     alias msakura='cd /Volumes/sakura'
-    ;;
-linux*)
-    alias vi='vim'
-    export EDITOR=vim
-    ;;
-esac
 
-# EasyEther Setting -TO BE CONFIG ANDRIOD BY ADB
-if [ -f /System/Library/Extensions/EasyTetherUSBEthernet.kext ]; then
+    # EasyEther Setting -TO BE CONFIG ANDRIOD BY ADB
     EASYTETHER_EXT=/System/Library/Extensions/EasyTetherUSBEthernet.kext
     alias easytether-on="sudo kextload $EASYTETHER_EXT"
     alias easytether-off="sudo kextunload $EASYTETHER_EXT"
     alias easytether-status="kextstat |grep EasyTether"
-fi
+    ;;
+linux*)
+    # vim
+    alias vi='vim'
+    export EDITOR=vim
+    ;;
+esac
 
 # Bash Color
 export PS1='\u@\[\033[1;31m\]\H\[\033[0m\][\W]\$ '
--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -5,15 +5,29 @@ alias lf="ls -F"
 alias ll="ls -l"
 
 # misc command alias
-#alias ls='ls -G -F'
-alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
-#alias cemacs='emacs-23.3'
-alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
-alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+case "${OSTYPE}" in
+darwin*)
+    # vim
+    alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+    alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+
+    # emacs
+    alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
+
+    # MacFusion alias
+    alias miacc='cd /Volumes/test_iacc'
+    alias msakura='cd /Volumes/sakura'
+
+    # EasyEther Setting -TO BE CONFIG ANDRIOD BY ADB
+    EASYTETHER_EXT=/System/Library/Extensions/EasyTetherUSBEthernet.kext
+    alias easytether-on="sudo kextload $EASYTETHER_EXT"
+    alias easytether-off="sudo kextunload $EASYTETHER_EXT"
+    alias easytether-status="kextstat |grep EasyTether"
+linux*)
+    #vim
+    alias vi='vim'
+    export EDITOR=vim
+esac
 alias scrr='screen -U -D -RR'
 alias s='screen -U'
 # alias nave='~/nave/nave.sh'
-
-# MacFusion alias
-# alias miacc='cd /Volumes/test_iacc'
-# alias msakura='cd /Volumes/sakura'
