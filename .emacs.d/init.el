314a44ba8a0026e2734c0d6860c635de6027aa5d
toqoz <toqoz403@gmail.com>
2012-01-23 16:28:30 +0900

    emacs設定ちょっとだけ

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+;; add load path
+(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp"))
+
 ; set japanese
 (set-language-environment 'Japanese)
 ; set UTF-8
@@ -8,43 +12,46 @@
 (setq make-backup-files nil)
 ; .#* don't create backup
 (setq auto-save-default nil)
-; use option for meta key
+; [for mac] use option for meta key
 (setq mac-option-modifier 'meta)
+
 ; font setting
-(when (>= emacs-major-version 23)
- (set-face-attribute 'default nil
-                     :family "monaco"
-                     :height 140)
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0208
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0212
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'mule-unicode-0100-24ff
-  '("monaco" . "iso10646-1"))
- (setq face-font-rescale-alist
-      '(("^-apple-hiragino.*" . 1.2)
-        (".*osaka-bold.*" . 1.2)
-        (".*osaka-medium.*" . 1.2)
-        (".*courier-bold-.*-mac-roman" . 1.0)
-        (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-        (".*monaco-bold-.*-mac-roman" . 0.9)
-        ("-cdac$" . 1.3))))
-;;(set-frame-font "ricty-13")
+(set-frame-font "ricty-12")
 ;;(set-frame-font (if (boundp 'window-system)
 
+;; window setting
 (if (boundp 'window-system)
-    (setq initial-frame-alist
-          (append (list
-                  '(width . 100)
-                  '(height . 50)
-                  '(top . 0)
-                  '(left . 150)
-                  )
-          initial-frame-alist)))
+  (setq initial-frame-alist
+    (append (list
+        '(width . 100)
+        '(height . 50)
+        '(top . 0)
+        '(left . 150))
+      initial-frame-alist)))
 (setq default-frame-alist initial-frame-alist)
+
+;; use space insted of tab
+(setq indent-tabs-mode nil)
+(setq tab-width 2 indent-tabs-mode nil)
+
+;; show line feed code
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+;; show line number
+;; (require 'linum)
+;; (global-linum-mode t)
+;; (setq linum-format "%5d")
+;;
+(require 'nyan-mode)
+(nyan-mode)
+(nyan-start-animation)
+
+(global-set-key (kbd "M-<RET>") 'ns-toggle-fullscreen)
+
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (setq auto-install-use-wget t)
+  (auto-install-compatibility-setup))
