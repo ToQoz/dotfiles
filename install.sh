7b842bc009283062d41158aa883b9e336e7e5a74
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-10-28 23:43:28 +0900

    Add submodule `antigen`

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-
-#!/bin/bash
-
-DOT_FILES=( .zsh .zshenv .zshrc .bashrc .bash_profile .vim .vimrc .gvimrc .vimperatorrc .vimperator .emacs.d .gitconfig .gitignore_global .gitmodules .screenrc .tmux.conf .tm_properties )
+DOT_FILES=(
+    .zsh .zshenv .zshrc
+    .bashrc .bash_profile
+    .vim .vimrc .gvimrc
+    .vimperatorrc .vimperator
+    .emacs.d
+    .gitconfig .gitignore_global .gitmodules
+    .screenrc
+    .tmux.conf
+    .tm_properties
+)
 
 for file in ${DOT_FILES[@]}
 do
