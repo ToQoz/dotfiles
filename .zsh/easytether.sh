88e7e03e5d0148fce371b23d9a9a3953f373b797
Takatoshi Matsumoto <toqoz403@gmail.com>
2014-08-13 18:56:42 +0900

    Drop .zsh/easytether.sh

--- .zsh/easytether.sh
+++
@@ -1,5 +0,0 @@
-#EasyEther Setting -TO BE CONFIG ANDRIOD BY ADB
-EASYTETHER_EXT=/System/Library/Extensions/EasyTetherUSBEthernet.kext
-alias easytether-on="sudo kextload $EASYTETHER_EXT"
-alias easytether-off="sudo kextunload $EASYTETHER_EXT"
-alias easytether-status="kextstat |grep EasyTether"
