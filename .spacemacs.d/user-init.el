1224b21f15abdd33d5aa2839027d4ddab67bfb7b
Takatoshi Matsumoto <toqoz403@gmail.com>
2017-03-29 23:12:19 +0900

    spacemacs: ignore custom-file

--- .spacemacs.d/user-init.el
+++ .spacemacs.d/user-init.el
@@ -3,6 +3,6 @@
 (setq exec-path-from-shell-check-startup-files nil)
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
-                                        ; custom-file
-(setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+; custom-file
+(setq custom-file (expand-file-name "custom-file.el" dotspacemacs-directory))
 (load custom-file 'no-error 'no-message)
