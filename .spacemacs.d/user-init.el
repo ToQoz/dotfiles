243c0f6fd55f33b1a028f4d065e5aa8234e79645
Takatoshi Matsumoto <toqoz403@gmail.com>
2017-04-07 13:48:16 +0900

    spacemacs: add more path

--- .spacemacs.d/user-init.el
+++ .spacemacs.d/user-init.el
@@ -2,8 +2,21 @@
 (setq tramp-ssh-controlmaster-options
       "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
 
+; ensime
 (push '("melpa-stable" . "stable.melpa.org/packages/") configuration-layer--elpa-archives)
 (push '("ensime" . "melpa-stable") package-pinned-packages)
+
+; exec-path
 (setq exec-path-from-shell-check-startup-files nil)
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-(setq exec-path (append exec-path '("/usr/local/bin")))
+(let* ((path '(
+               "/usr/local/bin"
+               "/usr/local/opt/coreutils/libexec/gnubin"
+               "/usr/local/opt/gnu-tar/libexec/gnubin"
+               "~/.rbenv/shims"
+               "~/_go/bin"
+               "~/bin"
+               "~/private/bin"
+               )))
+  (dolist (p path)
+    (add-to-list 'exec-path (expand-file-name p))
+    (setenv "PATH" (concat (expand-file-path p) ":" (getenv "PATH")))))
