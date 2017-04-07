e966c97f404bfe7c1a1087db3fa58462783ddc22
Takatoshi Matsumoto <toqoz403@gmail.com>
2017-04-07 13:41:50 +0900

    spacemacs: style

--- .spacemacs.d/_layers.el
+++ .spacemacs.d/_layers.el
@@ -54,8 +54,7 @@
            shell-default-position 'bottom)
     spell-checking
     syntax-checking
-    version-control
-    )
+    version-control)
   ;; List of additional packages that will be installed without being
   ;; wrapped in a layer. If you need some configuration for these
   ;; packages, then consider creating a layer. You can also put the
