91c21a338a5eba2675b602747b3f59a6b7d76b31
Takatoshi Matsumoto <toqoz403@gmail.com>
2017-04-07 13:41:25 +0900

    spacemacs: eshell

--- .spacemacs.d/_layers.el
+++ .spacemacs.d/_layers.el
@@ -48,9 +48,10 @@
                     :disabled-for  ; layer name
                     org
                     markdown)
-    ;; (shell :variables
-    ;;        shell-default-height 30
-    ;;        shell-default-position 'bottom)
+    (shell :variables
+           shell-default-shell 'eshell
+           shell-default-height 30
+           shell-default-position 'bottom)
     spell-checking
     syntax-checking
     version-control
